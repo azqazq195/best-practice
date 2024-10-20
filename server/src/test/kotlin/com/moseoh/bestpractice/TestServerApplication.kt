da231424f365cbb5e47110b1eba5007ad62a470b
문성하 <azqazq195@gmail.com>
2024-10-20 17:05:58 +0900

    remove: server

--- server/src/test/kotlin/com/moseoh/bestpractice/TestServerApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.moseoh.bestpractice
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-    fromApplication<ServerApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
